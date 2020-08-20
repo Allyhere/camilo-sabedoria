c783d2496181b49509df489b9a5b3ab2421eda35
allyhere <camilomichelettors@gmail.com>
2020-08-19 22:51:50 -0300

    style alterations suggested by one of the best programmers i know

--- index.js
+++ index.js
@@ -1,42 +1,42 @@
 const frase = document.getElementById("frase");
 const bg = document.getElementById("main");
-const {random, floor} = Math;
+const { random, floor } = Math;
 const twitter = document.getElementById("twitter");
 const clickspace = document.getElementById("click");
 const { throttle } = _;
 
-let typewriter = new Typewriter(frase, {
+const typewriter = new Typewriter(frase, {
   loop: false,
   delay: 30,
-  autoStart: true
+  autoStart: true,
 });
 
-clickspace.addEventListener("click", function(){
+clickspace.addEventListener("click", function () {
   throtled();
-})
+});
 
-window.addEventListener('keypress', function(ev){
-  if(ev.keyCode == 32) throtled();
-})
+window.addEventListener("keypress", function (ev) {
+  if (ev.keyCode === 32) throtled();
+});
 
-const fetchFrase =  () =>{
-  console.log("hey")
-  fetch('./frases.json')
-  .then(res => res.json())
-  .then(json =>{
-    randNum = floor(random() * json.frases.length);
-    randImg = floor(random() * 2) + 1;
-    randPhrase = json.frases[randNum];
-    twitter.href = `https://twitter.com/intent/tweet?url=http%3A%2F%2Fallyhere.github.io%2Fcamilo-sabedoria&via=lixeletto&text=${randPhrase}&hashtags=sabedoria`;
-    typePhrase(randPhrase);
-    bg.style.background = `rgb(6,6,6) url('./assets/${randImg}.png') no-repeat bottom right`;
-    bg.style.backgroundSize = "contain";
-  })
-}
+const fetchFrase = () => {
+  console.log("hey");
+  fetch("./frases.json")
+    .then((res) => res.json())
+    .then((json) => {
+      randNum = floor(random() * json.frases.length);
+      randImg = floor(random() * 2) + 1;
+      randPhrase = json.frases[randNum];
+      twitter.href = `https://twitter.com/intent/tweet?url=http%3A%2F%2Fallyhere.github.io%2Fcamilo-sabedoria&via=lixeletto&text=${randPhrase}&hashtags=sabedoria`;
+      typePhrase(randPhrase);
+      bg.style.background = `rgb(6,6,6) url('./assets/${randImg}.png') no-repeat bottom right`;
+      bg.style.backgroundSize = "contain";
+    });
+};
 
 const throtled = throttle(fetchFrase, 6000);
 
-function typePhrase(string){
+function typePhrase(string) {
   typewriter
     .deleteAll()
     .pauseFor(200)
@@ -44,7 +44,5 @@ function typePhrase(string){
     .pauseFor(1000)
     .start();
 }
-
-
 
 fetchFrase();
